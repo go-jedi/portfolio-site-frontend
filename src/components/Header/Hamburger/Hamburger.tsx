0d0d9071cfa3e0d767e6ba3fc31cc7688bd4e315
go-jedi <golangjedi@gmail.com>
2024-04-30 20:56:30 +0300

    add close hamburger menu if click link

--- src/components/Header/Hamburger/Hamburger.tsx
+++ src/components/Header/Hamburger/Hamburger.tsx
@@ -6,19 +6,30 @@ import styles from "@/components/Header/Hamburger/Hamburger.module.scss";
 import Menu from "@/components/Header/Hamburger/Menu/Menu";
 
 const Hamburger: React.FC = () => {
+    const [isChecked, setIsChecked] = React.useState<boolean>(false)
     const [isOpen, setIsOpen] = React.useState<boolean>(false);
 
-    const onClickHamBurger = () => {
+    const onClickHamburger = () => {
         try {
             setIsOpen((prev) => !prev);
+            setIsChecked((prev) => !prev);
 
             if (!isOpen) {
-                document.body.style.overflow = "hidden"
+                document.body.style.overflow = "hidden";
             } else {
-                document.body.style.overflow = "auto"
+                document.body.style.overflow = "auto";
             }
         } catch (error) {
-            console.log("error in onClickHamBurger", error);
+            console.log("error in onClickHamburger:", error);
+        }
+    }
+
+    const handleCloseHamburger = (): void => {
+        try {
+            setIsOpen(false)
+            setIsChecked(false)
+        } catch (error) {
+            console.log("error in handleCloseHamburger:", error)
         }
     }
 
@@ -27,7 +38,8 @@ const Hamburger: React.FC = () => {
             <div className={styles.hamburger}>
                 <input
                     className={styles.checkbox}
-                    onClick={onClickHamBurger}
+                    onChange={onClickHamburger}
+                    checked={isChecked}
                     type="checkbox"
                     id="hamburger-checkbox"
                 />
@@ -37,7 +49,9 @@ const Hamburger: React.FC = () => {
                 />
             </div>
             {
-                isOpen && <Menu/>
+                isOpen && <Menu
+                    handleCloseHamburger={handleCloseHamburger}
+                />
             }
         </div>
     )
--- src/components/Header/Hamburger/Menu/Menu.tsx
+++ src/components/Header/Hamburger/Menu/Menu.tsx
@@ -5,6 +5,10 @@ import {usePathname} from 'next/navigation'
 
 import styles from "@/components/Header/Hamburger/Menu/Menu.module.scss";
 
+type MenuProps = {
+    handleCloseHamburger: () => void;
+}
+
 type LinksType = {
     id: number;
     name: string;
@@ -34,7 +38,7 @@ const links: LinksType[] = [
     }
 ]
 
-const Menu: React.FC = () => {
+const Menu: React.FC<MenuProps> = (props: MenuProps) => {
     const pathname: string = usePathname()
 
     return (
@@ -46,6 +50,7 @@ const Menu: React.FC = () => {
                     <li className={styles.item} key={e.id}>
                         <Link
                             className={styles.link}
+                            onClick={props.handleCloseHamburger}
                             style={{color: pathname === e.link ? '#4385d7' : ''}}
                             key={e.id}
                             href={`${e.link}`}
